9e054942d1186ef9f5cdb0d3ec88f8b681f822fe
nathanpmyoung <nathanpmyoung@gmail.com>
2024-10-31 21:33:07 -0700

    fix vercel

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'identified_only',
